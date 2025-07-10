65fa769367edd573b94ee84c1111fd7dbee0b1aa
Tim Mayer <45499910+MayerT1@users.noreply.github.com>
2025-07-10 15:00:56 -0500

    dev

---
+++ main.js
@@ -0,0 +1,18 @@
+var routine = require("users/tjm0042/AM_Colab:routine.js");
+var indices = require("users/tjm0042/AM_Colab:indices.js");
+var s1Correction = require("users/tjm0042/AM_Colab:s1_correction.js");
+var model = require("users/tjm0042/AM_Colab:model.js");
+// var modelTuner = require("users/biplov/bhutan-aces-v-1:model_tuner.js");
+var utils = require("users/tjm0042/AM_Colab:/utils.js");
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+exports.routine = routine;
+exports.indices = indices;
+exports.s1Correction = s1Correction;
+exports.model = model;
+// // exports.modelTuner = modelTuner;
+exports.utils = utils;
+
